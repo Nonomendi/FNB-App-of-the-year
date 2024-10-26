9420bb399e79f7b8d12a2c130ded0181ca20df94
Thulani Tyaphile <tj.tyaphile@gmail.com>
2024-10-26 08:02:15 +0200

    Project setup structure

---
+++ AppOfTheYearServer/src/test/java/za/co/appoftheyear/appoftheyearserver/AppOfTheYearServerApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.appoftheyear.appoftheyearserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppOfTheYearServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
